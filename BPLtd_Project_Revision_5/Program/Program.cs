1fb044af1d5851205baa0268c797b37e9aa23edb
Jacob Lummis <jl1143@student.aru.ac.uk>
2022-12-06 19:44:26 +0000

    Started Revision 5

---
+++ BPLtd_Project_Revision_5/Program/Program.cs
@@ -0,0 +1,54 @@
+﻿// ----------------------------------------------------------
+// 'Broken Petrol Ltd' Petrol Station Management Application
+//  Version: 0
+// ----------------------------------------------------------
+//  Code by Jacob Lummis
+//  ARU-P SID: 2213276
+// ----------------------------------------------------------
+// Main Program File
+
+// Initialise user interface and subordinate threads
+userInterface thisInterface = new userInterface();
+thisInterface.userChoice = 0;
+Thread Navigator = new Thread(thisInterface.keyReader);
+int vehicleCount = 0;
+
+// Declare vehicle list and index vehicle
+List<vehicle> vehiclePool = new List<vehicle>();
+vehicle indexVehicle = new vehicle();
+
+// Initialise Timer
+System.Timers.Timer aTimer;
+
+// Main Program Body
+Console.WriteLine("");
+Navigator.Start();
+while(thisInterface.userChoice != 1)
+{
+    // Call vehicle generating timer
+    setTimer();
+
+
+    // 
+    aTimer.Stop();
+    aTimer.Dispose();
+}
+
+Console.WriteLine("\n\n\tPress any key to exit.");
+Console.ReadKey();
+
+// Program Methods
+void setTimer()
+{
+    aTimer = new System.Timers.Timer(1500);
+    //
+    aTimer.Elapsed += OnTimedEvent;
+    aTimer.AutoReset = true;
+    aTimer.Enabled = true;
+}
+
+void OnTimedEvent()
+{
+    indexVehicle = new vehicle();
+    vehiclePool.Add(indexVehicle);
+}
---
+++ BPLtd_Project_Revision_5/Program/classes/userInterface.cs
@@ -0,0 +1,22 @@
+// ----------------------------------------------------------
+// 'Broken Petrol Ltd' Petrol Station Management Application
+//  Version: 0
+// ----------------------------------------------------------
+//  Code by Jacob Lummis
+//  ARU-P SID: 2213276
+// ----------------------------------------------------------
+//
+class userInterface
+{
+    public int userChoice;
+    ConsoleKeyInfo userKey;
+    public void keyReader()
+    {
+        while(userChoice != 1)
+        {
+
+        }
+    }
+
+
+}
